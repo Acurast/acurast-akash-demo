729ea9b2b4957f7e896e0ff5ec6ceeb2b7dade13
Andreas Gassmann <andreas@andreasgassmann.ch>
2025-04-03 16:33:23 +0200

    fix: env variables

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,24 @@ import dotenv from "dotenv";
 // Load env vars once
 dotenv.config();
 
+declare const _STD_: {
+  env: {
+    RPC_ENDPOINT: string;
+    MNEMONIC: string;
+  };
+};
+
+if (typeof _STD_ === "undefined") {
+  (global as any)._STD_ = {
+    env: {
+      RPC_ENDPOINT: process.env.RPC_ENDPOINT || "",
+      MNEMONIC: process.env.MNEMONIC || "",
+    },
+  };
+}
+
 // Export configured values
 export const config = {
-  rpcEndpoint: process.env.RPC_ENDPOINT || "",
-  mnemonic: process.env.MNEMONIC || "",
-}; 
+  rpcEndpoint: _STD_.env.RPC_ENDPOINT || "",
+  mnemonic: _STD_.env.MNEMONIC || "",
+};
